34177bdad22c33ccf745655b105b8b9b357d24be
Renan Franca <renanfrancaveiculos@gamil.com>
2026-09-24 10:27:04 -0300

    feat: criação de array e integração com input no front-end

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
+const input = document.querySelector('input');
 const button = document.querySelector('button');
+const list = document.querySelector('ul');
 
 
-async function handle () {
-    const response = await fetch ('http://localhost:3000');
-    const data = await response.json();
-    console.log(data);
+async function handle () {  
+    try {
+        const response = await fetch ('http://localhost:3000/' + input.value);
+        const datas = await response.json();
+
+        if (!response.ok) {
+            throw new Error(response.status + '. Rota não existente.');
+        }
+        
+        list.textContent = '';
+    
+    
+        for (let i = 0; i < datas.length; i++) {
+            const item = document.createElement('li')
+            item.textContent = (datas[i].name || datas[i].produto) + ' | ' + (datas[i].email || datas[i].valor);
+            list.appendChild(item);
+        }
+
+    } catch (error) {
+        list.textContent = 'Erro ' + error.message;
+    }
 }
 
 button.addEventListener ('click', handle);
--- server.js
+++ server.js
@@ -1,13 +1,50 @@
 const http = require('node:http');
 
 http.createServer((request, response) => {
-  response.setHeader ('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Origin', '*');
+
+  if (request.url === '/users') {
+    response.writeHead(
+      200,
+      { 'content-type': 'application/json' }
+    );
+    response.end(JSON.stringify([{
+      name: 'Alex Bessa',
+      email: 'alex@email.com'
+    }, {
+      name: 'Renan França',
+      email: 'Renan@email.com'
+    },
+    {
+      name: 'Renê França',
+      email: 'Renê@email.com'
+    }]));
+    return;
+
+  }
+  if (request.url === '/products') {
+    response.writeHead(
+      200,
+      { 'content-type': 'application/json' }
+    );
+    response.end(JSON.stringify([{
+      produto: 'notebook',
+      valor: '10.000'
+    }, {
+      produto: 'Iphone',
+      valor: '7.200'
+    },
+    {
+      produto: 'tablet',
+      valor: '3.500'
+    }]));
+    return;
+  }
   response.writeHead(
-    200, 
-    {'content-type': 'application/json'}
-);
-  response.end(JSON.stringify({
-    name: 'Alex Bessa',
-    email: 'alex@email.com'
-  }));
+    404,
+    { 'content-type': 'aplication/json' }
+  );
+  response.end(JSON.stringify({ message: 'Não Existente.' }));
+  return;
+
 }).listen(3000);
